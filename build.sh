8bcec9d5bc522ecad8accab8045efbf4dff2a7ea
preetbajaj0101 <preetbajaj200@gmail.com>
2025-11-19 20:12:01 +0530

    super

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+if [[$CREATE_SUPERUSER]];
+then
+    python manage.py createsuperuser --noinput
+fi
